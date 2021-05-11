aa9eae7bc7f6eb71c4b7853ae73157ebc9c1a71b
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-05-12 02:28:00 +0900

    プロジェクト作成

---
+++ src/Detail.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class Detail extends Component {
+
+
+    render() {
+        return (
+            <p>Loading...</p>
+        );
+    }
+}
+
+export default Detail;
---
+++ src/List.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class List extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false
+        };
+    }
+
+    componentWillMount() {
+        if (this.state.loading) {
+            return;
+        }
+
+        document.title = '反応一覧';
+
+        let url = 'https://chemist.swiswiswift.com/resource/reactions.json';
+        return fetch(url)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    loading: true,
+                    data: responseJson,
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+                alert(error.toString());
+            });
+    }
+
+
+    render() {
+
+        if (!this.state.loading) {
+            return (
+                <div className="App-header">
+                    <p>Loading...</p>
+                </div>
+            );
+        }
+
+        return (
+            <div className="App-header">
+                <ul>
+                    {this.state.data.map(value => (
+                        <a href={"/reaction/" + value['directoryName']}>
+                            <div>
+                                <h2>{value['directoryName']}</h2>
+                                <img
+                                    src={"https://chemist.swiswiswift.com/resource/images/" + value['directoryName'] + "/" + value['thmbnailName']}/>
+                            </div>
+                        </a>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default List;
